fb6bd74db5d4f979ec236da45a90c351e3f3bc47
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-06-15 11:13:31 +0700

    done

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import model.Login;
+import model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import service.UserDAO;
+
+@Controller
+public class UserController {
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("home", "login", new Login());
+        return modelAndView;
+    }
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+        User user = UserDAO.checkLogin(login);
+        ModelAndView modelAndView;
+        if(user == null){
+            modelAndView = new ModelAndView("error");
+        } else {
+            modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+        }
+        return modelAndView;
+    }
+}
